fedcc9ed38dd9fe78689a6aa7aac4f26696dadcb
Ndata <wisdomndata@gmail.com>
2026-01-06 14:48:27 +0100

    Signup

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -3,31 +3,24 @@ import React from 'react'
 const Page = () => {
   return (
     <section className="w-full max-w-full mx-auto
-      lg:h-[915px] lg:bg-green-500  lg:flex 
-    lg:flex-row lg:justify-center lg:items-center">
-
-         {/* <section className="
-      w-full 
-      max-w-[1440px] 
-      mx-auto 
-      lg:h-[585px] 
-      xl:h-[915px]
-      lg:flex 
-      lg:justify-center 
-      lg:items-center
-    "></section>
-       */}
+      lg:h-[915px] lg:bg-green-500  lg:flex lg:flex-col lg:justify-center lg:items-center">
       {/* Left box */}
-      <div className="lg:w-full max-w-full lg:h-[585px] lg:bg-[#b4e5f3] lg:p-16">
+      <div className='lg:w-full max-w-full lg:flex bg-red-300 lg:h-[755px] lg:mt-3'>
+        <div className="lg:w-full max-w-full lg:h-[755px] lg:bg-[#b4e5f3] lg:p-16">
+             <div className='lg:w-[194px] lg:h-[51px] lg:bg-amber-600 lg:-mt-10 lg:items-center lg:justify-center
+      l'>
+        <h2 className='lg:text-[40px] lg:font-medium  lg:text-[#0079C6] lg:items-center lg:justify-center'>AbroadEx</h2></div>
         <div>
 
         </div>
       </div>
 
       {/* Right box */}
-      <div className="lg:w-full max-w-full lg:h-[585px] lg:bg-[#F8FAFC] lg:flex lg:justify-center lg:items-center">
-        {/* Content for right box */}
+      <div className="lg:w-full max-w-full lg:h-[755px] lg:bg-[#3c7dbd] lg:flex lg:justify-center lg:items-center">
+      
       </div>
+      </div>
+      
 
     </section>
   )
