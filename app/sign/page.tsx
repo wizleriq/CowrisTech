85b5698d14ef5fa4cd3557e6524732131960f328
Ndata <wisdomndata@gmail.com>
2026-01-08 20:19:29 +0100

    Signup

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import Goggle from '@/app/src/img/Google.png'
+import Apple from '@/app/src/img/Apple.png'
+import Study from '@/app/src/img/Study.png'
+import Image from 'next/image'
 
 const Page = () => {
   return (
     <section className="w-full max-w-full mx-auto
-      lg:h-[915px] lg:bg-green-500 lg:flex lg:flex-col lg:justify-center lg:items-center">
+      lg:h-[1000px] lg:bg-green-500 lg:flex lg:flex-col lg:justify-center lg:items-center">
       {/* Left box */}
-      <div className='lg:w-full max-w-full lg:flex bg-red-300 lg:h-[820px]'>
-        <div className="lg:w-full max-w-full lg:h-[820px] lg:bg-[#F6FDFF] lg:px-12 ">
+      <div className='lg:w-full max-w-full lg:flex bg-red-300 lg:h-[1000px]'>
+        <div className="lg:w-full max-w-full lg:h-[1000px] lg:bg-[#F6FDFF] lg:px-12 lg:flex lg:items-center lg:justify-center ">
              {/* <div className='lg:w-[194px] lg:h-[51px] lg:bg-amber-600 lg:mx-auto lg:mb-4 lg:flex lg:items-center lg:justify-center'>
         <h2 className='lg:text-[40px] lg:font-medium  lg:text-[#a2abb1] lg:flex lg:items-center lg:justify-center'>AbroadEx</h2></div> */}
-         <div className="lg:w-full max-w-full lg:h-[755px] lg:bg-[#ffffff] lg:p-16 flex-col lg:flex lg:items-center">
+         <div className="lg:w-full max-w-full lg:h-[890px] lg:bg-[#ffffff] lg:p-16 flex-col lg:flex lg:items-center">
           {/* Create Account Starts */}
           <div className='lg:w-[276px] lg:h-14 bg-transparent flex flex-col justify-between item-center  lg:flex lg:-mt-13'>
             <div className='lg:w-[276px] lg-[36px] bg-transparent flex lg:justify-center '>
               <h2 className='lg:text-[28.66px] lg:font-medium lg:leading-[100%]'>Create an account </h2>
            </div>
-           <div className='lg:w-[147px] lg:h-[19.69px] bg-transparent flex lg:justify-center item-center mx-auto '>
+           <div className='lg:w-[147px] lg:h-[19.69px] bg-transparent lg:mt-2 flex lg:justify-center item-center mx-auto '>
             <h2 className='lg:text-[16.77px] lg:font-medium lg:leading-[100%]'>Enter Your Details</h2>
            </div>
           </div>
@@ -154,7 +158,7 @@ const Page = () => {
     placeholder="Enter your password"
     required
   />
-  <p className="text-xs text-gray-500 mt-1">
+  <p className="text-xs text-gray-500 mt-1 lg:text-[11.74px] lg:font-medium">
     Must contain at least 8 characters, a special character, and a number
   </p>
 </div>
@@ -169,8 +173,13 @@ const Page = () => {
       className="lg:w-[531.93px] lg:h-[40px] lg:px-3 lg:text-sm lg:rounded-md lg:border lg:border-[#AAB2CC] lg:outline-none"
       required
     />
-     <p className="text-xs text-gray-500 mt-1">
-    I agree to the Terms & Condition and Privacy Policy
+     <p className="text-xs text-gray-500 mt-1 lg:text-[11.74px] lg:font-medium">
+    
+    <label className="flex items-center gap-2">
+  <input type="checkbox" className="form-checkbox h-4 w-4">
+</input>
+  <span >I agree to the <span className='text-[#0079C6]'>Terms & Condition</span> and <span className='text-[#0079C6]'>Privacy Policy</span></span>
+</label>
   </p>
   </div>
 
@@ -178,10 +187,58 @@ const Page = () => {
   <div className="flex justify-center lg:mt-6">
     <button
       type="submit"
-      className="bg-[#FF8600] text-white lg:w-[389px] lg:h-[40px] lg:rounded-md lg:text font-medium"
+      className="bg-[#0079C6] text-white lg:w-[389px] lg:h-[40px] lg:rounded-md lg:text-[20.12px] font-medium"
     >
       Sign Up
     </button>
+  </div>
+  
+
+  <div className='lg:w-[531.93px]  lg:bg-transparent lg:h-3.5 lg:flex lg:flex-row lg:justify-between lg:text-center lg:items-center'>
+    <div className='lg:w-[170px]  lg:h-px lg:bg-[#C0C3CD]'>
+
+    </div>
+
+    <div className='lg:w-[120.4px] lg:h-[13.3px] lg:bg-transparentlg:flex lg:text-center lg:items-center lg:justify-center'>
+      <h2 className="text-gray-500 lg:text-[12.6px] lg:font-medium lg:text-center lg:items-center lg:justify-center">Or sign up with</h2>
+    </div>
+   
+     <div className='lg:w-[170px]  lg:h-px lg:bg-[#C0C3CD]'>
+
+    </div>
+
+
+  </div>
+
+  <div className="lg:w-[531.93px] lg:h-10 lg:px-3 lg:text-sm lg:flex lg:justify-between lg:items-center lg:bg-transparent">
+     <div className="lg:w-[137.1px] lg:h-[31px] lg:px-8 lg:text-sm lg:rounded-md lg:bg-transaparent lg:border
+     lg:flex lg:justify-between lg:items-center lg:border-[#AAB2CC] lg:outline-none">
+         <div className="relative lg:w-[13px] lg:flex lg:justify-between lg:items-center lg:bg-transparent lg:h-[9.4px]">
+         <Image
+      src={Goggle}
+      alt="Profile Image"
+      fill
+      className="object-fill lg:w-[13px] lg:h-[9.4px] lg:justify-between lg:items-center"   /* or object-contain */
+    />
+    </div>
+     <h2> Google</h2>
+    </div>
+    <div className="lg:w-[137.1px] lg:h-[31px] lg:px-8 lg:text-sm lg:rounded-md lg:bg-transaparent lg:border
+     lg:flex lg:justify-between lg:items-center lg:border-[#AAB2CC] lg:outline-none">
+         <div className="relative lg:w-[13px] lg:flex lg:justify-between lg:items-center lg:bg-transparent lg:h-[9.4px]">
+         <Image
+      src={Apple}
+      alt="Profile Image"
+      fill
+      className="object-fill lg:w-[13px] lg:h-[9.4px] lg:justify-between lg:items-center"   /* or object-contain */
+    />
+    </div>
+     <h2>Apple</h2>
+    </div>
+  </div>
+
+  <div className='lg-mx-auto lg:bg-transparent lg:w-[531.93px] lg:flex lg:items-center lg:justify-center lg:h-6'>
+    <h2 className=''>Already have an account? <span className='text-[#0079C6]'>Sign In</span></h2>
   </div>
 
 </form>
@@ -285,8 +342,23 @@ const Page = () => {
       </div>
 
       {/* Right box */}
-      <div className="lg:w-full max-w-full lg:h-[755px] lg:bg-[#3c7dbd] lg:flex lg:justify-center lg:items-center">
-      
+      <div className="lg:w-full max-w-full lg:h-[1000px] lg:bg-[#ffffff] lg:flex lg:justify-center lg:items-center">
+      <div className="lg:w-[442px] lg:h-[539px] max-w-full lg:bg-[#ffffff] lg:rounded-md">
+        {/* width: 442.72344970703125;
+height: 539.5689086914062;
+angle: 0 deg;
+opacity: 1;
+top: 102px;
+left: 849px;
+border-radius: 4px; */}
+
+       <Image
+      src={Study}
+      alt="Profile Image"
+      className="object-filllg lg:w-[442px] lg:h-[539px] lg:justify-between lg:items-center"   /* or object-contain */
+    />
+
+      </div>
       </div>
       </div>
       
@@ -296,34 +368,3 @@ const Page = () => {
 }
 
 export default Page
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// const page = () => {
-//   return (
-//         // <section className="w-full lg:h-[585px] max-w-[1440px] lg:bg-green-500 lg:flex lg:flex-row lg:justify-center lg:items-center">
-//            <section className="lg:w-full lg:h-[585px] max-w-[2000px] lg:bg-green-500 lg:flex lg:flex-row lg:justify-center lg:items-center">
-
-//            <div className='lg:w-[690px] lg:h-[585px] lg:bg-[#FFFFFF] lg:p-16'>
-
-//             </div>
-//           <div className='lg:w-[710px] lg:h-[585px] lg:bg-[#F8FAFC] lg:flex lg:justify-center lg:items-center '>
-//             </div>
-//     </section>
-    
-//   )
-// }
-
-// export default page
-
-// width: 959;
-// height: 1024;
-// angle: 0 deg;
-// opacity: 0.48;
